e9d484558c38a29843cd9ba6cde8bcf29aa4c70e
Skynuke <kosmos-omel@yandex.ru>
2024-12-01 23:01:04 +0300

    метод Ферма

--- dema/Source.cpp
+++ dema/Source.cpp
@@ -1,4 +1,18 @@
 #include<iostream>
+double my_power(double val, int p) {
+    if (p < 0) {
+        return my_power(1 / val, -p); 
+    }
+    if (p == 0) {
+        return 1;
+    }
+    if (p % 2 == 0) {
+        return my_power(val * val, p / 2); 
+    }
+    else {
+        return val * my_power(val * val, (p - 1) / 2); 
+    }
+}
 int main() {
 
 	return 0;
